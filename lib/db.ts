3762b6ad3b54e4d7168d2fd18f5514faa4d7bc89
SharkieBate9898 <jjx2121@gmail.com>
2026-03-23 22:26:57 -0400

    fix: revert to standard libsql client to evade Nextjs fetch polyfill stream error

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client/web";
+import { createClient } from "@libsql/client";
 import { type LeadStage, leadStages } from "@/lib/leadStages";
 
 console.log("[db] CWD =", process.cwd());
